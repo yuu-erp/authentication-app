a2d0848a1cc277b55f5bffd04bbd645744716b12
Lê Khải Hoàn <129830151+yuu-erp@users.noreply.github.com>
2024-09-30 20:14:08 +0700

    UPDATE

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,7 +2,7 @@ import { IConfiguration } from "./configuration.type";
 
 export const configuration = (): IConfiguration => ({
   nodeEnv: process.env.NODE_ENV || 'development',
-  port: Number(process.env.PORT) || 3001,
+  port: Number(process.env.APP_PORT) || 3001,
   cloudinaryClundName: process.env.CLOUDINARY_CLOUD_NAME || '',
   cloudinaryApiKey: process.env.CLOUDINARY_API_KEY || '',
   cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET || '',
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigurationService } from './config/configuration.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(6000);
+  const configurationService = app.get(ConfigurationService);
+  const port = configurationService.port;
+  console.log("port: ", port)
+  await app.listen(port);
 }
 bootstrap();
